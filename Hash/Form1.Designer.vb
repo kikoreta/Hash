12133000936a5fc377378e3c2f1ebbb5895b260d
kikoreta <enr.gtz@outlook.com>
2018-03-05 19:04:51 -0600

    v1.0

--- Hash/Form1.Designer.vb
+++ Hash/Form1.Designer.vb
@@ -22,9 +22,144 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(229, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 51)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(33, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "MD5:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox2.Location = New System.Drawing.Point(64, 48)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(207, 20)
+        Me.TextBox2.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 84)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "SHA1:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 117)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "SHa256:"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox3.Location = New System.Drawing.Point(64, 81)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(207, 20)
+        Me.TextBox3.TabIndex = 9
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox4.Location = New System.Drawing.Point(64, 114)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ReadOnly = True
+        Me.TextBox4.Size = New System.Drawing.Size(207, 20)
+        Me.TextBox4.TabIndex = 10
+        '
+        'Button2
+        '
+        Me.Button2.AutoSize = True
+        Me.Button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.Button2.Location = New System.Drawing.Point(247, 10)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(26, 23)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "..."
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(64, 140)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(207, 20)
+        Me.TextBox5.TabIndex = 12
+        '
+        'Timer1
+        '
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 143)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(52, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Compara:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
-    End Sub
+        Me.ClientSize = New System.Drawing.Size(283, 170)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.Text = "Calcular Hash, MD5"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
 
+    End Sub
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label4 As Label
 End Class
--- Hash/Form1.vb
+++ Hash/Form1.vb
@@ -1,3 +1,105 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports System.Security.Cryptography
 
+Public Class Form1
+
+    Function hash_generator(ByVal hash_type As String, ByVal file_name As String)
+
+        Dim hash
+
+        If hash_type.ToLower = "md5" Then
+            hash = MD5.Create
+        ElseIf hash_type.ToLower = "sha1" Then
+            hash = SHA1.Create()
+        ElseIf hash_type.ToLower = "sha256" Then
+            hash = SHA256.Create()
+        Else
+            MsgBox("Unknown type of hash : " & hash_type, MsgBoxStyle.Critical)
+            Return False
+        End If
+
+        Dim hashValue() As Byte
+        Dim fileStream As FileStream = File.OpenRead(file_name)
+        fileStream.Position = 0
+        hashValue = hash.ComputeHash(fileStream)
+        Dim hash_hex = PrintByteArray(hashValue)
+        fileStream.Close()
+        Return hash_hex
+
+    End Function
+
+    Public Function PrintByteArray(ByVal array() As Byte)
+
+        Dim hex_value As String = ""
+        Dim i As Integer
+        For i = 0 To array.Length - 1
+            hex_value += array(i).ToString("X2")
+        Next i
+        Return hex_value.ToLower
+    End Function
+
+    Function md5_hash(ByVal file_name As String)
+        Return hash_generator("md5", file_name)
+    End Function
+
+    Function sha_1(ByVal file_name As String)
+        Return hash_generator("sha1", file_name)
+    End Function
+
+    Function sha_256(ByVal file_name As String)
+        Return hash_generator("sha256", file_name)
+    End Function
+
+    Public ActiveTextBox As TextBox
+
+    Private Sub TextBox_Enter(sender As Object, e As EventArgs) Handles TextBox2.Enter, TextBox3.Enter, TextBox4.Enter
+        ActiveTextBox = CType(sender, TextBox)
+    End Sub
+
+    Private Sub TextBox_MouseDown() Handles TextBox2.MouseDown, TextBox3.MouseDown, TextBox4.MouseDown
+
+        ActiveTextBox.SelectAll()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim openfile As New OpenFileDialog()
+        If (openfile.ShowDialog = DialogResult.OK) Then
+            TextBox1.Text = openfile.FileName
+            Dim path As String = TextBox1.Text
+            TextBox2.Text = md5_hash(path)
+            TextBox3.Text = sha_1(path)
+            TextBox4.Text = sha_256(path)
+        End If
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Select Case TextBox5.Text
+            Case TextBox2.Text
+                TextBox3.BackColor = Color.White
+                TextBox4.BackColor = Color.White
+                TextBox2.BackColor = Color.Green
+                TextBox5.BackColor = Color.Green
+            Case TextBox3.Text
+                TextBox2.BackColor = Color.White
+                TextBox4.BackColor = Color.White
+                TextBox3.BackColor = Color.Green
+                TextBox5.BackColor = Color.Green
+            Case TextBox4.Text
+                TextBox2.BackColor = Color.White
+                TextBox3.BackColor = Color.White
+                TextBox4.BackColor = Color.Green
+                TextBox5.BackColor = Color.Green
+            Case ""
+                TextBox2.BackColor = Color.White
+                TextBox3.BackColor = Color.White
+                TextBox4.BackColor = Color.White
+                TextBox5.BackColor = Color.White
+            Case Else
+                TextBox2.BackColor = Color.White
+                TextBox3.BackColor = Color.White
+                TextBox4.BackColor = Color.White
+                TextBox5.BackColor = Color.Red
+        End Select
+    End Sub
 End Class
